5342c5da5b68025db907231615e5532ff2ab5401
gitmvfs <marcossantos@gmail.com>
2024-03-12 23:57:51 -0300

    feat: att e delete tarefa
    feito teste unitário nas rotas de atualizar e deletar tarefa

--- src/routes/tarefas.js
+++ src/routes/tarefas.js
@@ -1,4 +1,4 @@
-const { recuperarTodasTarefas, recuperarTarefaPelaCategoria, criarTarefa } = require("../controller/tarefas")
+const { recuperarTodasTarefas, recuperarTarefaPelaCategoria, criarTarefa, atualizarTarefa, deletarTarefa } = require("../controller/tarefas")
 
 const router = require("express").Router()
 
@@ -36,12 +36,57 @@ router.get("/tarefa/categoria", async (req, res) => {
 router.post("/tarefa", async (req, res) => {
 
     try {
-        let { titulo, descricao, concluido, nomeCategoria } = req.body
+        const { titulo, descricao, concluido, nomeCategoria } = req.body
         const dataPrevistaTermino = new Date(req.body.dataPrevistaTermino)
         // concluido = new Boolean(concluido)
 
         const response = await criarTarefa(titulo, dataPrevistaTermino, descricao, concluido, nomeCategoria)
-        res.json(response).status(200)
+
+        response.data == 0
+            ? res.json({ msg: "Erro ao criar tarefa", code: 400, response }).status(400)
+            : res.json({ msg: "Tarefa criada com sucesso", code: 200, response }).status(200)
+
+    }
+    catch (err) {
+        res.json({ err: err, code: 500 }).status(500)
+
+    }
+
+})
+
+
+router.put("/tarefa", async (req, res) => {
+
+    try {
+        const { id, titulo, descricao, concluido } = req.body
+        const dataPrevistaTermino = new Date(req.body.dataPrevistaTermino)
+        // concluido = new Boolean(concluido)
+
+        const response = await atualizarTarefa(id, titulo, dataPrevistaTermino, descricao, concluido)
+
+        response.data == 0
+            ? res.json({ msg: "Erro ao atualizar tarefa", code: 400, response }).status(400)
+            : res.json({ msg: "Tarefa atualizada com sucesso", code: 200, response }).status(200)
+
+    }
+    catch (err) {
+        res.json({ err: err, code: 500 }).status(500)
+
+    }
+
+})
+
+router.delete("/tarefa", async (req, res) => {
+
+    try {
+        const { id } = req.body
+        const dataPrevistaTermino = new Date(req.body.dataPrevistaTermino)
+
+        const response = await deletarTarefa(id)
+
+        response.data == 0
+            ? res.json({ msg: "Erro ao deletar tarefa", code: 400, response }).status(400)
+            : res.json({ msg: "Tarefa deletada com sucesso", code: 200, response }).status(200)
 
     }
     catch (err) {
